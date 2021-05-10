9e7ca7fa85ec5414bcc15115e793947c4e7896a4
cheiw256 <cheiw256@gmail.com>
2021-05-10 15:38:29 +0800

    Initial commit

---
+++ src/Student.java
@@ -0,0 +1,39 @@
+public class Student {
+    private int rep;
+    private int dive;
+    private int lunchStart;
+    private int lunchPeriod;
+
+    public int getRep() {
+        return rep;
+    }
+
+    public void setRep(int rep) {
+        this.rep = rep;
+    }
+
+    public int getDive() {
+        return dive;
+    }
+
+    public void setDive(int dive) {
+        this.dive = dive;
+    }
+
+    public int getLunchStart() {
+        return lunchStart;
+    }
+
+    public void setLunchStart(int lunchStart) {
+        this.lunchStart = lunchStart;
+    }
+
+    public int getLunchPeriod() {
+        return lunchPeriod;
+    }
+
+    public void setLunchPeriod(int lunchPeriod) {
+        this.lunchPeriod = lunchPeriod;
+    }
+
+}
